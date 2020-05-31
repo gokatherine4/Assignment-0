313817413af983a2ae68901d6c0442dd8682dec2
gokatherine4 <go.katherine4@gmail.com>
2020-05-31 19:44:24 -0400

    sumOfNumsWithinRange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,14 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+
+  let count = 0;
+
+  for (let x =0; x <nums.legnth; x++){
+  if (nums[x] >= start && nums[x] <= end){
+    count++;
+  }
+}
+return count;
 }
 
 // Do not edit this line;
