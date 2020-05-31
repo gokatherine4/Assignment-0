a833c18a0c721477cf9ef35764b6a5571aeb2f7e
gokatherine4 <go.katherine4@gmail.com>
2020-05-31 19:58:42 -0400

    git add .

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,11 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+
+  let min = Math.mins(...nums);
+  let max = Math.max(...nums);
+  return min+max;
 }
+
 
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
