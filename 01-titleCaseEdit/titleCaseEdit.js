8be83641c505714710cac603cbae672b60647c19
gokatherine4 <go.katherine4@gmail.com>
2020-05-31 15:48:26 -0400

    titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,11 @@
 function titleCaseEdit(title) {
   // Insert code here;
+text = title. split (" ");
+for (var i = 0 ; i < text.length; i++){
+  text[i] = text[i],charAt(0).toUpperCase(0) + text[i].substr(1);
+}
+text = text.join(" ");
+return text;
 }
 
 // Do not edit this line;
