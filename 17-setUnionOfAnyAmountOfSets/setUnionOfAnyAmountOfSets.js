4cf4589b0dba8abceb1c62b36a5f9833a9655f5a
gokatherine4 <go.katherine4@gmail.com>
2020-05-31 22:51:47 -0400

    setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,12 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  let mySet = new Set();
+  for (let i = 0; i < args.length; i++){
+    for (let element of args[i]){
+      mySet.add(element);
+    }
+  }
+  return mySet;
 }
 
 // Do not edit this line;
