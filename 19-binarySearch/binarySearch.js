f3249cc3591d3f9b352480129b3dbbf5aa5225ba
gokatherine4 <go.katherine4@gmail.com>
2020-06-01 00:26:59 -0400

    binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -3,12 +3,31 @@ class MySolution {
     this.flag = false; // this is optional to use;
   }
 
-  binarySearch(nums, target) {
+  binarySearch(nums, target){
     // Insert code here;
+  let start = 0;
+  let middle;
+  let end = nums.length -1; 
 
-  }
+while (start <= end)
+{
+  middle = Math.floor((start +end) / 2);
+if (nums[middle] === target)
+  return true;
+
+else{
+    if (nums[middle] > target)
+      end = middle -1;
+
+    else
+    start = middle +1;
+}
+}
+return false;
+    }
 }
 
-// Do not edit this line;
+
+ /// Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
