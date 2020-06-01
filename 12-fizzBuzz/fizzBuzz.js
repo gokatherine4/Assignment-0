fec590d77489948de09c0b5c7e3e08b0eba05d6e
gokatherine4 <go.katherine4@gmail.com>
2020-05-31 20:20:06 -0400

    fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,6 +1,23 @@
 function fizzBuzz(start, end) {
   // Insert code here;
+  let fizzBuzzArr = [];
+  
+  for (let i = start; i <= end; i ++){
+    if (i % 3 ==0 && i % 5 != 0) {
+      fizzBuzzArr.push("Fizz");
+    }
+    else if (i % 5 == 0 && i % 3 != 0) {
+      fizzBuzzArr.push("Buzz");
+    }
+    else if (i % 5 ==0 && i % 3 == 0) {
+      fizzBuzzArr.push("Fizz");
+    }
+    else
+    {
+fizzBuzzArr.push(i)
+  }
 }
-
+return fizzBuzzArr;
+}
 // Do not edit this line;
 module.exports = fizzBuzz;
